ad0f8e82498d651edb16ed1fec9c574ed033e3f8
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-01-25 11:13:13 -0800

    Vibrate

--- src/edu/uiuc/booksearch/SearchActivity.java
+++ src/edu/uiuc/booksearch/SearchActivity.java
@@ -1,20 +1,23 @@
 package edu.uiuc.booksearch;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Menu;
-import android.widget.TextView;
 
 public class SearchActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		Intent intent = getIntent();
-		String message = intent.getStringExtra(Main.EXTRA_MESSAGE);
-		((TextView)findViewById(R.id.query)).setText(message);
+		String query = intent.getStringExtra(Main.EXTRA_MESSAGE);
+		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		v.vibrate(500);
 	}
 
 	@Override
