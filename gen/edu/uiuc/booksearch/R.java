090091ff06425c938cc6288107a237fc5e2293ad
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-01-25 12:45:20 -0800

    Morse vibrations

--- gen/edu/uiuc/booksearch/R.java
+++ gen/edu/uiuc/booksearch/R.java
@@ -29,16 +29,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int button1=0x7f090003;
-        public static final int isbn=0x7f090002;
-        public static final int query=0x7f090004;
-        public static final int textView0=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int action_settings=0x7f090003;
+        public static final int button1=0x7f090002;
+        public static final int isbn=0x7f090001;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_search=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/edu/uiuc/booksearch/Main.java
+++ src/edu/uiuc/booksearch/Main.java
@@ -1,19 +1,63 @@
 package edu.uiuc.booksearch;
 
+import java.util.HashMap;
+
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
 public class Main extends Activity {
-    public final static String EXTRA_MESSAGE = "edu.uiuc.booksearch.isbn";
+	public final static String EXTRA_MESSAGE = "edu.uiuc.booksearch.isbn";
+	public HashMap<Character, String> mcode = new HashMap<Character, String>();
+
+	public void initMorse() {
+		mcode.put('a', ".-");
+		mcode.put('b', "-...");
+		mcode.put('c', "-.-.");
+		mcode.put('d', "-..");
+		mcode.put('e', ".");
+		mcode.put('f', "..-.");
+		mcode.put('g', "--.");
+		mcode.put('h', "....");
+		mcode.put('i', "..");
+		mcode.put('j', ".---");
+		mcode.put('k', "-.-");
+		mcode.put('l', ".-..");
+		mcode.put('m', "--");
+		mcode.put('n', "-.");
+		mcode.put('o', "---");
+		mcode.put('p', ".--.");
+		mcode.put('q', "--.-");
+		mcode.put('r', ".-.");
+		mcode.put('s', "...");
+		mcode.put('t', "-");
+		mcode.put('u', "..-");
+		mcode.put('v', "...-");
+		mcode.put('w', ".--");
+		mcode.put('x', "-..-");
+		mcode.put('y', "-.--");
+		mcode.put('z', "--..");
+		mcode.put('0', "-----");
+		mcode.put('1', ".----");
+		mcode.put('2', "..---");
+		mcode.put('3', "...--");
+		mcode.put('4', "....-");
+		mcode.put('5', ".....");
+		mcode.put('6', ".----");
+		mcode.put('7', "..---");
+		mcode.put('8', "...--");
+		mcode.put('9', "....-");
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initMorse();
 	}
 
 	@Override
@@ -22,12 +66,28 @@ public class Main extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	public void onClick(View view) {
-		Intent intent = new Intent(this, SearchActivity.class);
-		EditText editText = (EditText) findViewById(R.id.isbn);
-		intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
-		startActivity(intent);
+		String str = ((EditText) findViewById(R.id.isbn)).getText().toString();
+		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		int MAXLENGTH = 1000;
+		long[] pattern = new long[MAXLENGTH];
+		for (int i = 0; i < pattern.length; ++i)
+			pattern[i] = 0;
+		int i = 1;
+		for (char letter : str.toCharArray()) {
+			letter = Character.toLowerCase(letter);
+			String dnd = mcode.get((Character)letter);
+			for (char d : dnd.toCharArray()) {
+				if (d == '.')
+					pattern[i++] = 200;
+				else if (d == '-')
+					pattern[i++] = 600;
+				pattern[i++] = 200;
+			}
+			pattern[i - 1] = 600;
+		}
+		v.vibrate(pattern, -1);
 	}
 
 }
