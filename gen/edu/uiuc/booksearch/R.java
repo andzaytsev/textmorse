0acf48d9430315f1ae07207be26e4b149e91c84f
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-01-25 07:01:29 -0800

    Added project files

--- gen/edu/uiuc/booksearch/R.java
+++ gen/edu/uiuc/booksearch/R.java
@@ -29,9 +29,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_settings=0x7f090005;
         public static final int button1=0x7f090003;
         public static final int isbn=0x7f090002;
+        public static final int query=0x7f090004;
         public static final int textView0=0x7f090000;
         public static final int textView1=0x7f090001;
     }
