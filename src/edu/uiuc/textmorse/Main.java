2c5ffcee26556bafd21161c63b20c60b93e81e39
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-01-25 19:29:44 -0800

    Now saving the state of the button

--- src/edu/uiuc/textmorse/Main.java
+++ src/edu/uiuc/textmorse/Main.java
@@ -1,14 +1,15 @@
 package edu.uiuc.textmorse;
 
-
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class Main extends Activity {
-
+	public static final String PREFS_NAME = "MorseTextPrefs";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +17,19 @@ public class Main extends Activity {
 		setContentView(R.layout.activity_main);
 		Intent intent = new Intent(this, TextMorseService.class);
 		startService(intent);
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean display = settings.getBoolean("display", true);
+
+		if (!display)
+			finish();
 	}
-	
+
 	public void onClick(View view) {
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putBoolean("display",
+				!((CheckBox) findViewById(R.id.checkBox1)).isChecked());
+		editor.commit();
 		finish();
 	}
 
